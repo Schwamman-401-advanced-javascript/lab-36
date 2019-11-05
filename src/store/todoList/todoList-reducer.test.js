17915907ab162b955463dd1b09cfebdd86404d54
schwamman <schwamman@msn.com>
2019-11-05 09:34:00 -0600

    Fixed tests for updated reducers

--- src/store/todoList/todoList-reducer.test.js
+++ src/store/todoList/todoList-reducer.test.js
@@ -3,22 +3,18 @@ import { addItem, deleteItem, toggleComplete } from './todoList-reducer';
 
 describe('TodoList Reducer', () => {
   it('can add an item', () => {
-    const state = {
-      todoList: [],
-    }
+    const state = [];
     Object.freeze(state);
     const item = { name: "item1"}
     let action = addItem(item);
 
     let updatedState = reducer(state, action);
 
-    expect(updatedState).toEqual({ todoList:[{ name: "item1" }] });
+    expect(updatedState).toEqual([{ name: "item1" }]);
   });
 
   it('can delete an item by _id', () => {
-    const state = {
-      todoList: [],
-    }
+    const state = [];
     Object.freeze(state);
     const item = { 
       name: "item1",
@@ -28,15 +24,15 @@ describe('TodoList Reducer', () => {
     let deleteID = deleteItem(12345);
 
     let addedState = reducer(state, add);
-    expect(addedState).toEqual({ todoList:[{ name: "item1", _id: 12345 }] });
+    expect(addedState).toEqual([{ name: "item1", _id: 12345 }]);
     
     let deletedState = reducer(addedState, deleteID)
-    expect(deletedState).toEqual({ todoList:[]});
+    expect(deletedState).toEqual([]);
   });
 
   it('can toggle if an item has been completed', () => {
 
-    const state = { todoList:[{ 
+    const state = [{ 
         name: "item1",
         _id: 12345,
         complete:false,
@@ -45,16 +41,16 @@ describe('TodoList Reducer', () => {
         name: "item2",
         _id: 55555,
         complete:true,
-      }]
-    };
+      }];
     Object.freeze(state);
 
     let updatedState = reducer(state, toggleComplete(12345));
     let updatedState2 = reducer(state, toggleComplete(55555));
 
-    expect(updatedState.todoList[0].complete).toEqual(true);
-    expect(updatedState2.todoList[0].complete).toEqual(false);
-    
+    expect(updatedState[0].complete).toEqual(true);
+    expect(updatedState[1].complete).toEqual(true);
+    expect(updatedState2[0].complete).toEqual(false);
+    expect(updatedState2[1].complete).toEqual(false);
   })
 
 })
